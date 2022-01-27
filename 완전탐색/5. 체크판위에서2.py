8e5952c7d928305902dc5da610c20e53fffa50b6
Wonjun <velpegor@naver.com>
2022-01-27 19:34:44 +0900

    완전탐색I

---
+++ 완전탐색/5. 체크판위에서2.py
@@ -0,0 +1,16 @@
+a, b = map(int,input().split())
+arr = [
+    list(input().split()) for i in range(a)
+]
+
+cnt = 0
+
+for i in range(1, a):
+    for j in range(1, b):
+        for k in range(i+1, a-1):
+            for l in range(j+1,b-1):
+                if arr[0][0] != arr[i][j] and \
+                        arr[i][j] != arr[k][l] and arr[k][l] != arr[a-1][b-1]:
+                    cnt +=1
+
+print(cnt)
